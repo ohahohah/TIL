3d43332bcc5d3540ee37aa00460eebd608c3bdd9
ohahohah <ohahohah.dev@gmail.com>
2019-02-09 21:52:05 +0900

    Add squash command for commit log

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -1,7 +1,8 @@
 git checkout gh-pages
 
 # opt01. before push to remote 
-git merge master --no-edit 
+git merge --squash master --no-edit 
+git commit -m "squash and merge master"
 
 # opt02. Publish remote repository contents 
 #git pull origin gh-pages --rebase
